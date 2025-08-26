447bc98a1aeea2db85eca564b67b5b9ac587bb66
Enzo Cuba <enzocuba2005@gmail.com>
2025-08-26 20:38:54 -0300

    Formulas y HTML básico terminados.

---
+++ Conversor Temperatura/conversorTemperatura.js
@@ -0,0 +1,7 @@
+const valorCelsius = 35//document.getElementById('inputTemperatura');
+const celciusParaFahrenheit = (valorCelsius * 1.8) + 32;
+const celciusParaKelvin = valorCelsius + 273.15;
+
+console.log(valorCelsius);
+console.log(celciusParaFahrenheit);
+console.log(celciusParaKelvin);
