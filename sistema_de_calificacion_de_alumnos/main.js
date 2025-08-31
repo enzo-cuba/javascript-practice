a6c64ffcc64d62ecda0463cbeb396609a80ce8ab
Enzo Cuba <enzocuba2005@gmail.com>
2025-08-31 16:26:43 -0300

    Algunos avances...

--- sistema_de_calificacion_de_alumnos/main.js
+++ sistema_de_calificacion_de_alumnos/main.js
@@ -1,9 +1,20 @@
 const formData = document.getElementById("studentInput");
-const container = document.getElementById("studentInfo");
+const container = document.getElementById("studentResume");
+const obj = {
+    nombre: "",
+    notas: [],
+    promedio: 0,
+    estado: null
+}
 
 formData.addEventListener('submit', (event) =>{
     event.preventDefault();
     const data = new FormData(formData);
-    const objeto = Object.fromEntries(data.entries());
+    const name = data.get("student");
+    const score = data.getAll("score");
+    obj.nombre = name;
+    obj.notas = score.map(nota => Number(nota));
+    const promedio = parseFloat(obj.notas.reduce((acc, num) => acc + num, 0) / obj.notas.length).toFixed(2);
+    obj.promedio = Number(promedio);
+    obj.estado = promedio >= 60 ? "Aprobado" : "Reprobado";
 })
-
