5d8c33bb3da6177679bf9afd256e486f572a9200
Enzo Cuba <enzocuba2005@gmail.com>
2025-08-31 10:11:15 -0300

    Completado: Imprimir lista y validar inputs.

--- calculadora_de_notas/calculadoraDeNotas.js
+++ calculadora_de_notas/calculadoraDeNotas.js
@@ -1,12 +1,27 @@
-let userInput = document.getElementById("userInput").value;
-let scores = [userInput];
-let list = document.getElementById("list");
+const userInput = document.getElementById("userInput");
+const btn = document.getElementById("btn")
+const list = document.getElementById("list");
+
+function imprimir(score){
+    score = Number(userInput.value);
+    if (score <= 100 && typeof score === "number") {
+        const li = document.createElement("li")
+        li.textContent = "Nota: " + score;
+        list.appendChild(li);
+    } else {
+        alert("Ingrese un número válido.")
+    }
 
 
-scores.forEach(score => {
-    document.getElementById("btn").addEventListener('click', () => {
-    const li = document.createElement("li")
-    li.textContent = scores;
-    list.appendChild(li);
-    })
-})
+}
+
+
+
+btn.addEventListener('click', imprimir);
+
+
+//    document.getElementById("btn").addEventListener('click', () => {
+//    const li = document.createElement("li")
+//    li.textContent = scores;
+//    list.appendChild(li);
+//    })
