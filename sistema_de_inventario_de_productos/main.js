cf0689d63b2ec31e3ec0efcd3a479c8349f24655
Enzo Cuba <enzocuba2005@gmail.com>
2025-09-05 20:22:00 -0300

    Archivos y estrucutra del DOM listos

---
+++ sistema_de_inventario_de_productos/main.js
@@ -0,0 +1,39 @@
+/* 
+Funcionalidades:
+
+Agregar productos
+
+- Inputs: nombre, categoría, cantidad, precio
+
+- Validar que los datos sean correctos (nombre no vacío, cantidad y precio números válidos)
+
+-  Guardar cada producto en un array de objetos
+
+Mostrar productos
+
+- Mostrar todos los productos en una lista o tabla
+
+- Actualizar el DOM dinámicamente cada vez que se agregue un producto
+
+Filtrado dinámico
+
+- Input de búsqueda por nombre parcial o completo
+
+- La lista se filtra mientras el usuario escribe, mostrando solo los productos que coincidan
+
+Eliminar un producto
+
+- Cada producto podría tener un botón “Eliminar” que borre el objeto del array y del DOM
+
+- Editar un producto existente
+
+- Cargar los datos del producto en los inputs
+
+- Modificar valores y actualizar el objeto en el array y en la lista del DOM
+
+Filtrado por categoría
+
+- Agregar un <select> con categorías
+
+- Filtrar la lista según la categoría seleccionada
+*/
